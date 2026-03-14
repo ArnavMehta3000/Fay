f52fa47a914ec02dc442a73d8554e4161f6c4896
ArnavMehta3000 <arnavdatt@gmail.com>
2026-03-14 19:39:57 +0400

    Separate SimpleMath for linux compilation

--- xmake.lua
+++ xmake.lua
@@ -16,10 +16,12 @@ set_policy("build.warning", true)
 set_policy("run.autobuild", true)
 set_policy("package.install_locally", true)
 
-add_rules("plugin.compile_commands.autoupdate", {outputdir = ".vscode", lsp = "clangd"})
+add_rules("plugin.compile_commands.autoupdate", { outputdir = ".vscode", lsp = "clangd" })
 
 -- Add compilation success to all targets
 add_tests("CompileSuccess", { build_should_pass = true, group = "Compilation" })
+
+includes("External/SimpleMath/xmake.lua")
 
 -- For nvrhi
 add_repositories("MyRepo https://github.com/ArnavMehta3000/xmake-repo.git")
@@ -35,6 +37,7 @@ add_defines("FAY_ENABLE_PROFILING=" .. (is_mode("profile") and "1" or "0"))
 
 if is_plat("linux") then
 	set_toolchains("clang")
+	add_requires("directx-headers")
 elseif is_plat("windows") then
 	set_toolchains("msvc")
 end
@@ -43,43 +46,38 @@ target("Fay")
 	set_kind("binary")
 
 	add_packages("nvrhi", "sdl3", "tracy", "fastgltf", "stb", "nlohmann_json")
-
+	add_deps("SimpleMath")
+	
 	add_undefines("min", "max")
 	if is_plat("windows") then
 		add_defines("UNICODE", "_UNICODE", "NOMINMAX", "NOMCX", "NOSERVICE", "NOHELP", "WIN32_LEAN_AND_MEAN")
 	end
 
-	add_includedirs(
-		"Fay",
-		"External/SimpleMath",
-		{ public = true })
+	add_includedirs("Fay", { public = true })
 
-	add_files(
-		"Fay/**.cpp", 
-		"External/SimpleMath/**.cpp")
-	add_headerfiles(
-		"Fay/**.h",
-		"External/SimpleMath/**.h",
-		"External/SimpleMath/**.inl")
+	add_files("Fay/**.cpp")
+	add_headerfiles("Fay/**.h")
 
 	add_extrafiles("Shaders/**.hlsl", "Shaders/**.hlsli")
 
 	if is_plat("windows") then
-		add_linkdirs(path.join(os.getenv("VULKAN_SDK"),"Lib"))
+		add_linkdirs(path.join(os.getenv("VULKAN_SDK"), "Lib"))
 		add_links("dxgi", "vulkan-1")  -- Other links handled by nvrhi
 	elseif is_plat("linux") then
+	    add_packages("directx-headers")
 		add_links("vulkan")
+
 		remove_files("Fay/Graphics/DX12/**.cpp")
 		remove_headerfiles("Fay/Graphics/DX12/**.h")
 	end
 
-	after_build(function (target)
+	after_build( function (target)
 		import("core.project.task")
 		task.run("compile_shaders",
 		{
-			out_dir     = target:targetdir(),
-			src_path    = path.join(os.projectdir(), "Shaders"),
-			cfg_file    = path.join(os.projectdir(), "Shaders", "ShaderBuild.json"),
+			out_dir = target:targetdir(),
+			src_path = path.join(os.projectdir(), "Shaders"),
+			cfg_file = path.join(os.projectdir(), "Shaders", "ShaderBuild.json"),
 			target_name = target:name()
 		})
 
